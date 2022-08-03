736bdfd1434ee0f3fd34cd6388468e8437078d1f
Divdude <divyanshuverma91@gmail.com>
2022-08-03 10:59:30 +0400

    Update verbs.py

--- verbs.py
+++ verbs.py
@@ -19,49 +19,65 @@ def verb_check(text):
 
     # Convert all upper case statements to lower case
     for i in range(len(data)):
-        if data[i][0].isupper():
+        if data[i][1] not in ["NN", "NNP"]:
             data[i][0] = data[i][0].lower()
 
     # Check for abbreviations/slang not recognized by nltk
-    if len(data) > 1:
-        if data[0][0] == "ur":
-            data[:1] = [["u", "PRP"], ["r", "VBP"]]
-        if data[0][0] == "im":
-            data[:1] = [["i", "PRP"], ["r", "VBP"]]
-        if data[0][0] == "ive":
-            data[:1] = [["i", "PRP"], ["ve", "VBP"]]
-        if data[0][0] == "ik":
-            data[:1] = [["i", "PRP"], ["know", "VBP"]]
-        if data[0][0] in ["i", "u"]:
-            data[0][1] = "PRP"
-        if data[1][0] in ["suck", "sucks"]:
-            data[1][1] = "VBP"
-        if data[1][0] in ["r"]:
-            data[1][1] = "VBP"
+    for i in range(len(data)):
+        if data[i][0] == "ur":
+            data[i:i+1] = [["u", "PRP"], ["r", "VBP"]]
+        if data[i][0] == "im":
+            data[i:i+1] = [["i", "PRP"], ["am", "VBP"]]
+        if data[i][0] == "ive":
+            data[i:i+1] = [["i", "PRP"], ["ve", "VBP"]]
+        if data[i][0] == "ik":
+            data[i:i+1] = [["i", "PRP"], ["know", "VBP"]]
+        if data[i][0] in ["i", "u"]:
+            data[i][1] = "PRP"
+        if data[i][0] in ["suck", "sucks"]:
+            data[i][1] = "VBP"
+        if data[i][0] == "r":
+            data[i][1] = "VBP"
+        if data[i][0] in ["me", "i"]:
+            data[i][0] = "you"
+        if data[i][0] == "my":
+            data[i][0] = "your"
+        if data[i][0] == "mine":
+            data[i][0] = "yours"
+        if data[i][0] == "myself":
+            data[i][0] = "yourself"
 
-        # Change grammar for verbs to match "my mom"
-        if data[0][1] in ["PRP", "NNP", "NN", "WP"] and data[1][1] in ["VBP", "VBZ", "VBD", "VBN", "MD", "VBG"]:
-            if data[1][0] in ["am", "are", "'m", "'re", "r", "'s"]:
-                data[1][0] = "is"
-            elif data[1][0] in ["have", "ve", "'ve"]:
-                data[1][0] = "has"
-            if data[1][0][-1] == "o":
-                data[1][0] += "es"
-            if data[1][0] == "'ll":
-                data[1][0] = "will"
-            elif data[1][0][-1] != "s":
-                if data[1][1] == "VBP" and data[1][1] != "MD":
-                    data[1][0] += "s"
-            try:
-                if data[2][0] == "n't":
-                    data[2][0] = "not"
-            except IndexError:
-                pass
-            output = ""
-            for j in [i[0] for i in data[1:]]:
-                output += j + " "
-            return True, output.strip()
-        else:
-            return False, None
+    # Change grammar for verbs to match "my mom"
+    for j in range(len(data)):
+        try:
+            print("in")
+            if data[j][1] in ["PRP", "NNP", "NN", "WP"] and data[j+1][1] in ["VBP", "VBZ", "VBD", "VBN", "MD", "VBG"]:
+                if data[j+1][0] in ["am", "are", "'m", "'re", "r", "'s"]:
+                    data[j+1][0] = "is"
+                elif data[j+1][0] in ["have", "ve", "'ve"]:
+                    data[j+1][0] = "has"
+                if data[j+1][0][-1] == "o":
+                    data[j+1][0] += "es"
+                if data[j+1][0] == "'ll":
+                    data[j+1][0] = "will"
+                elif data[j+1][0][-2:] == "ss":
+                    data[j+1][0] += "es"
+                elif data[j+1][0][-1] != "s":
+                    if data[j+1][1] == "VBP" and data[j+1][1] != "MD":
+                        data[j+1][0] += "s"
+                try:
+                    if data[j+2][0] == "n't":
+                        data[j+2][0] = "not"
+                except IndexError:
+                    pass
+                del data[:j+1]
+                output = ""
+                for k in [i[0] for i in data]:
+                    output += k + " "
+                return True, output.strip()
+            else:
+                continue
+        except IndexError:
+            pass
     else:
         return False, None
