03f8e9c47360bd1a058e15935b7b2357a1948b6c
Divyanshu Verma <divyanshuverma91@gmail.com>
2025-07-26 14:10:09 +0400

    nltk updates

--- verbs.py
+++ verbs.py
@@ -1,8 +1,8 @@
 import nltk
 
 # Some nltk requirements
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
+nltk.download('punkt_tab')
+nltk.download('averaged_perceptron_tagger_eng')
 
 
 def verb_check(text):
